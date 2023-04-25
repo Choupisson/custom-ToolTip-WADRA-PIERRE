63651632ab8adb7078c270f762e9250d7bea9be2
Choupisson <108970143+Choupisson@users.noreply.github.com>
2023-04-25 11:21:57 +1100

    Tentative de rectification

--- src/app/custom-tooltip.directive.ts
+++ src/app/custom-tooltip.directive.ts
@@ -1,77 +1,57 @@
-import { Directive, ElementRef, HostListener, Input, Renderer2, AfterContentInit, ContentChild } from '@angular/core';
+import { Directive, ElementRef, Renderer2, Input, ContentChild, AfterViewInit, HostListener } from '@angular/core';
+
 
 @Directive({
-  selector: '[customTooltip]'
+  selector: '[customToolTip]'
 })
-export class CustomTooltipDirective implements AfterContentInit {
-  @Input('customTooltip') position: 'top' | 'bottom' | 'left' | 'right' | '' = 'top';
-  @ContentChild('customTooltipContent') contentElement?: ElementRef;
-  private tooltipElement?: HTMLElement;
+export class CustomToolTipDirective implements AfterViewInit {
+  @Input() position: 'top' | 'bottom' | 'left' | 'right' = 'top';
+  @ContentChild('customToolTipContent') customToolTipContent!: ElementRef;
+  private toolTipWrapper!: HTMLElement;
+  
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
-  @HostListener('mouseenter', ['$event'])
-  onMouseEnter(event: MouseEvent) {
-    this.showTooltip();
+  ngAfterViewInit() {
+    this.toolTipWrapper = this.renderer.createElement('div');
+    this.renderer.addClass(this.toolTipWrapper, 'tooltip-wrapper');
+    this.renderer.setStyle(this.toolTipWrapper, 'display', 'none');
+    this.renderer.appendChild(this.toolTipWrapper, this.customToolTipContent.nativeElement);
+    this.renderer.appendChild(this.el.nativeElement, this.toolTipWrapper);
   }
 
-  @HostListener('mouseleave', ['$event'])
-  onMouseLeave(event: MouseEvent) {
-    this.hideTooltip();
-  }
-
-  ngAfterContentInit() {
-    if (this.contentElement) {
-      this.renderer.setStyle(this.contentElement.nativeElement, 'display', 'none');
-    }
-  }
-
-  private showTooltip() {
-    if (!this.contentElement) {
-      return;
-    }
-    const tooltipContent = this.contentElement.nativeElement.innerHTML;
-
-    this.tooltipElement = this.renderer.createElement('span');
-    this.renderer.addClass(this.tooltipElement, 'custom-tooltip');
-    this.renderer.setProperty(this.tooltipElement, 'innerHTML', tooltipContent);
-    this.renderer.setStyle(this.tooltipElement, 'position', 'absolute');
-
+  @HostListener('mouseover')
+  onMouseOver() {
+    this.renderer.setStyle(this.toolTipWrapper, 'display', 'block');
     const hostRect = this.el.nativeElement.getBoundingClientRect();
+    const tooltipRect = this.toolTipWrapper.getBoundingClientRect();
     let top, left;
 
-    if (this.tooltipElement) {
-      switch (this.position) {
-        case 'top':
-          top = hostRect.top - this.tooltipElement.offsetHeight;
-          left = hostRect.left;
-          break;
-        case 'bottom':
-          top = hostRect.bottom;
-          left = hostRect.left;
-          break;
-        case 'left':
-          top = hostRect.top;
-          left = hostRect.left - this.tooltipElement.offsetWidth;
-          break;
-        case 'right':
-          top = hostRect.top;
-          left = hostRect.right;
-          break;
-      }
-
-      this.renderer.setStyle(this.tooltipElement, 'top', `${top}px`);
-      this.renderer.setStyle(this.tooltipElement, 'left', `${left}px`);
-
-      this.renderer.appendChild(this.el.nativeElement, this.tooltipElement);
+    switch (this.position) {
+      case 'top':
+        top = hostRect.top - tooltipRect.height;
+        left = hostRect.left;
+        break;
+      case 'bottom':
+        top = hostRect.bottom;
+        left = hostRect.left;
+        break;
+      case 'left':
+        top = hostRect.top;
+        left = hostRect.left - tooltipRect.width;
+        break;
+      case 'right':
+        top = hostRect.top;
+        left = hostRect.right;
+        break;
     }
+
+    this.renderer.setStyle(this.toolTipWrapper, 'top', `${top}px`);
+    this.renderer.setStyle(this.toolTipWrapper, 'left', `${left}px`);
   }
 
-  private hideTooltip() {
-    if (this.tooltipElement) {
-      this.renderer.removeChild(this.el.nativeElement, this.tooltipElement);
-      this.tooltipElement = undefined;
-    }
+  @HostListener('mouseout')
+  onMouseOut() {
+    this.renderer.setStyle(this.toolTipWrapper, 'display', 'none');
   }
 }
-
